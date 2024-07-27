b3b60c85bc99643c384bf64cf186e743e5a45f33
Swadhin2001 <2025.it18.swadhinpaul@gmail.com>
2024-07-27 11:50:11 +0530

    bug fixed

--- src/controller/TelegramBot.controller.ts
+++ src/controller/TelegramBot.controller.ts
@@ -5,24 +5,16 @@ const token = process.env.TELEGRAM_BOT_TOKEN as string;
 
 const telegramBot = async () => {
     try {
-        const bot = new TelegramBot(token, {polling: true});
-        bot.onText(/\/echo (.+)/, async (msg, match) => {
-            const chatId = msg.chat.id;
-            if (match) {
-                const resp = match[1]; 
-                bot.sendMessage(chatId, resp);
-            }          
-        });
-
-        bot.on('message', async (msg) => {
+        const bot = new TelegramBot(token, {polling: false});
+        
+        bot.startPolling();
+        bot.onText(/\/echo (.+)/, async (msg) => {
             const message = await aiResponse(msg.text as string);
             const chatId = msg.chat.id;
             bot.sendMessage(chatId, message as string);
-            console.log('message', msg.text);
-            console.log('Response', message);
-        });
+        });        
+        bot.stopPolling();
         
-
     } catch (error) {
         console.log("telegram bot api error: ", error);
     }
