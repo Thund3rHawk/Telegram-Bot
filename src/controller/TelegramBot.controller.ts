6ee5584f0864c3faae0b551058cbd8cfac753905
Swadhin2001 <2025.it18.swadhinpaul@gmail.com>
2024-07-27 11:23:38 +0530

    bug fixed

--- src/controller/TelegramBot.controller.ts
+++ src/controller/TelegramBot.controller.ts
@@ -2,19 +2,24 @@ import TelegramBot from "node-telegram-bot-api";
 import { aiResponse } from "../utils/aiResponse";
 
 const token = process.env.TELEGRAM_BOT_TOKEN as string;
-const bot = new TelegramBot(token, { polling: true });
 
 const telegramBot = async () => {
     try {
-        bot.onText(/\/echo (.+)/, async (msg) => {
-            const message = await aiResponse(msg.text as string);            
+        const bot = new TelegramBot(token);
+        bot.onText(/\/echo (.+)/, async (msg, match) => {
+            const message = await aiResponse(msg.text as string);
+            const chatId = msg.chat.id;
+            if (match) {
+                const resp = match[1]; 
+                bot.sendMessage(chatId, resp);
+            } 
 
             bot.on('message', async (msg) => {
                 const chatId = msg.chat.id;
                 bot.sendMessage(chatId, message as string);
             });
             console.log('message', msg.text);
-            console.log ('Response', message);            
+            console.log('Response', message);
         });
 
 
