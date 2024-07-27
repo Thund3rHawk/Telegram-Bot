399a613ff22dfcc29f89bb9c2ecdcfb6b3ec4eef
Swadhin2001 <2025.it18.swadhinpaul@gmail.com>
2024-07-27 11:17:59 +0530

    bug fixed

--- src/controller/TelegramBot.controller.ts
+++ src/controller/TelegramBot.controller.ts
@@ -1,23 +1,22 @@
-import { Request, Response } from "express";
 import TelegramBot from "node-telegram-bot-api";
 import { aiResponse } from "../utils/aiResponse";
 
+const token = process.env.TELEGRAM_BOT_TOKEN as string;
+const bot = new TelegramBot(token, { polling: true });
 
-const telegramBot = async (req: Request, res: Response) => {
-    const token = process.env.TELEGRAM_BOT_TOKEN as string;
-    const bot = new TelegramBot(token, { polling: false });
+const telegramBot = async () => {
     try {
         bot.onText(/\/echo (.+)/, async (msg) => {
-            const chatId = msg.chat.id;
-            const message = await aiResponse(msg.text as string);
-            bot.sendMessage(chatId, message as string);
+            const message = await aiResponse(msg.text as string);            
+
+            bot.on('message', async (msg) => {
+                const chatId = msg.chat.id;
+                bot.sendMessage(chatId, message as string);
+            });
+            console.log('message', msg.text);
+            console.log ('Response', message);            
         });
 
-        // bot.on('message', async (msg) => {
-        //     const chatId = msg.chat.id;
-        //     console.log("msg from telegram: ", msg);
-        //     bot.sendMessage(chatId, 'message recieved');
-        // });
 
     } catch (error) {
         console.log("telegram bot api error: ", error);
